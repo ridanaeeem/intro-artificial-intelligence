c5b5e190ec97235c37abbc18369773b5b8fd4149
ridanaeeem <ridan@bu.edu>
2024-04-03 10:24:08 -0400

    zombayes init

---
+++ src/labs/zombayes/agents/NaiveBayesAgent.java
@@ -0,0 +1,81 @@
+package src.labs.zombayes.agents;
+
+
+// SYSTEM IMPORTS
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+
+// JAVA PROJECT IMPORTS
+import edu.bu.labs.zombayes.agents.SurvivalAgent;
+import edu.bu.labs.zombayes.features.Features.FeatureType;
+import edu.bu.labs.zombayes.linalg.Matrix;
+import edu.bu.labs.zombayes.utils.Pair;
+
+
+
+public class NaiveBayesAgent
+    extends SurvivalAgent
+{
+
+    public static class NaiveBayes
+        extends Object
+    {
+
+        public static final FeatureType[] FEATURE_HEADER = {FeatureType.CONTINUOUS,
+                                                            FeatureType.CONTINUOUS,
+                                                            FeatureType.DISCRETE,
+                                                            FeatureType.DISCRETE};
+
+        // TODO: complete me!
+        public NaiveBayes()
+        {
+
+        }
+
+        // TODO: complete me!
+        public void fit(Matrix X, Matrix y_gt)
+        {
+            return;
+        }
+
+        // TODO: complete me!
+        public int predict(Matrix x)
+        {
+            return -1;
+        }
+
+    }
+    
+    private NaiveBayes model;
+
+    public NaiveBayesAgent(int playerNum, String[] args)
+    {
+        super(playerNum, args);
+        this.model = new NaiveBayes();
+    }
+
+    public NaiveBayes getModel() { return this.model; }
+
+    @Override
+    public void train(Matrix X, Matrix y_gt)
+    {
+        System.out.println(X.getShape() + " " + y_gt.getShape());
+        this.getModel().fit(X, y_gt);
+    }
+
+    @Override
+    public int predict(Matrix featureRowVector)
+    {
+        return this.getModel().predict(featureRowVector);
+    }
+
+}
