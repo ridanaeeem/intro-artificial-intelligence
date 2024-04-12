e0bb84d486b4bd534c88b997781c40b0d1666fd5
ridanaeeem <ridan@bu.edu>
2024-04-11 20:11:06 -0400

    fixed gaussian equation

--- src/labs/zombayes/agents/NaiveBayesAgent.java
+++ src/labs/zombayes/agents/NaiveBayesAgent.java
@@ -53,6 +53,11 @@ public class NaiveBayesAgent
         double f1HumanSum = 0;
         // sum of feature 1 for zombies
         double f1ZombieSum = 0;
+        // standard deviation of feature 1 for humans
+        double f1HumanSTDEV = 0;
+        // standard deviation of feature 1 for zombies
+        double f1ZombieSTDEV = 0;
+
         // mean of feature 2 for humans
         double f2HumanMean = 0;
         // mean of feature 2 for zombies
@@ -60,6 +65,12 @@ public class NaiveBayesAgent
         // sum of feature 2 for humans
         double f2HumanSum = 0;
         // sum of feature 2 for zombies
+        double f2ZombieSum = 0;
+        // standard deviation of feature 2 for humans
+        double f2HumanSTDEV = 0;
+        // standard deviation of feature 2 for zombies
+        double f2ZombieSTDEV = 0;
+
         // basically turn continuous into discrete
         // feature 1
         Pair <Double, Double> f1HumanStd1 = new Pair<Double, Double>(0.0, 0.0);
@@ -230,21 +241,6 @@ public class NaiveBayesAgent
             // CONTINUOUS features
             //
             // feature 1
-            // go through every row of y_gt
-            // increment corresponding value of feature 1
-            for (int i = 0; i < numDataPoints; i++) {
-                // if human
-                if (y_gt.get(i, 0) == 0) {
-                    f1HumanSum += X.get(i, 0);
-                // if zombie
-                } else if (y_gt.get(i, 0) == 1) {
-                    f1ZombieSum += X.get(i, 0);
-                }
-            }
-            f1HumanMean = f1HumanSum / totalHumans;
-            f1ZombieMean = f1ZombieSum / totalZombies;
-            // System.out.println("f1HumanMean " + f1HumanMean);
-            // System.out.println("f1ZombieMean " + f1ZombieMean);
             // standard deviation of feature 1 for humans
             double f1HumanStdDev = 0;
             // standard deviation of feature 1 for zombies
@@ -254,14 +250,22 @@ public class NaiveBayesAgent
             for (int i = 0; i < numDataPoints; i++) {
                 // if human
                 if (y_gt.get(i, 0) == 0) {
+                    f1HumanSum += X.get(i, 0);
                     f1HumanStdDev += Math.pow(X.get(i, 0) - f1HumanMean, 2);
                 // if zombie
                 } else if (y_gt.get(i, 0) == 1) {
+                    f1ZombieSum += X.get(i, 0);
                     f1ZombieStdDev += Math.pow(X.get(i, 0) - f1ZombieMean, 2);
                 }
             }
+            f1HumanMean = f1HumanSum / totalHumans;
+            f1ZombieMean = f1ZombieSum / totalZombies;
+            // System.out.println("f1HumanMean " + f1HumanMean);
+            // System.out.println("f1ZombieMean " + f1ZombieMean);
+            f1HumanSTDEV = (f1HumanStdDev / totalHumans);
             f1HumanStdDev = Math.pow((f1HumanStdDev / totalHumans),0.5);
-            f1ZombieStdDev =  Math.pow((f1HumanStdDev / totalHumans),0.5);
+            f1ZombieSTDEV = (f1ZombieStdDev / totalZombies);
+            f1ZombieStdDev =  Math.pow((f1ZombieStdDev / totalZombies),0.5);
             // System.out.println("f1HumanStdDev " + f1HumanStdDev);
             // System.out.println("f1ZombieStdDev " + f1ZombieStdDev);
             // ranges
@@ -281,20 +285,6 @@ public class NaiveBayesAgent
 
             // feature 2
             double f2ZombieSum = 0;
-            // go through every row of y_gt
-            for (int i = 0; i < numDataPoints; i++) {
-                // if human
-                if (y_gt.get(i, 0) == 0) {
-                    f2HumanSum += X.get(i, 1);
-                // if zombie
-                } else if (y_gt.get(i, 0) == 1) {
-                    f2ZombieSum += X.get(i, 1);
-                }
-            }
-            f2HumanMean = f2HumanSum / totalHumans;
-            f2ZombieMean = f2ZombieSum / totalZombies;
-            // System.out.println("f2HumanMean " + f2HumanMean);
-            // System.out.println("f2ZombieMean " + f2ZombieMean);
             // standard deviation of feature 2 for humans
             double f2HumanStdDev = 0;
             // standard deviation of feature 2 for zombies
@@ -303,13 +293,21 @@ public class NaiveBayesAgent
             for (int i = 0; i < numDataPoints; i++) {
                 // if human
                 if (y_gt.get(i, 0) == 0) {
+                    f2HumanSum += X.get(i, 1);
                     f2HumanStdDev += Math.pow(X.get(i, 1) - f2HumanMean, 2);
                 // if zombie
                 } else if (y_gt.get(i, 0) == 1) {
+                    f2ZombieSum += X.get(i, 1);
                     f2ZombieStdDev += Math.pow(X.get(i, 1) - f2ZombieMean, 2);
                 }
             }
+            f2HumanMean = f2HumanSum / totalHumans;
+            f2ZombieMean = f2ZombieSum / totalZombies;
+            // System.out.println("f2HumanMean " + f2HumanMean);
+            // System.out.println("f2ZombieMean " + f2ZombieMean);
+            f2HumanSTDEV = (f2HumanStdDev / totalHumans);
             f2HumanStdDev = Math.pow((f2HumanStdDev / totalHumans),0.5);
+            f2ZombieSTDEV = (f2ZombieStdDev / totalZombies);
             f2ZombieStdDev = Math.pow((f2ZombieStdDev / totalZombies),0.5);
             // System.out.println("f2HumanStdDev " + f2HumanStdDev);
             // System.out.println("f2ZombieStdDev " + f2ZombieStdDev);
@@ -326,7 +324,6 @@ public class NaiveBayesAgent
             // System.out.println("f2ZombieStd1 " + f2ZombieStd1.getFirst() + " " + f2ZombieStd1.getSecond());
             // System.out.println("f2ZombieStd2 " + f2ZombieStd2.getFirst() + " " + f2ZombieStd2.getSecond());
             // System.out.println("f2ZombieStd3 " + f2ZombieStd3.getFirst() + " " + f2ZombieStd3.getSecond());
-
 
             // System.out.println("f3HumanProbs " + f3HumanProbs);
             // System.out.println("f3ZombieProbs " + f3ZombieProbs);
@@ -350,62 +347,105 @@ public class NaiveBayesAgent
 
             double f1HumanProb = 0;
             double f1ZombieProb = 0;
-
-            if (f1 >= f1HumanStd1.getFirst() && f1 <= f1HumanStd1.getSecond()) {
-                f1HumanProb = .318;
-            } else if (f1 >= f1HumanStd2.getFirst() && f1 <= f1HumanStd2.getSecond()) {
-                f1HumanProb = .046;
-            } else if (f1 >= f1HumanStd3.getFirst() && f1 <= f1HumanStd3.getSecond()) {
-                f1HumanProb = .003;
-            } else {
-                f1HumanProb = .0001;
-            }
-
-            if (f1 >= f1ZombieStd1.getFirst() && f1 <= f1ZombieStd1.getSecond()) {
-                f1ZombieProb = .318;
-            } else if (f1 >= f1ZombieStd2.getFirst() && f1 <= f1ZombieStd2.getSecond()) {
-                f1ZombieProb = .046;
-            } else if (f1 >= f1ZombieStd3.getFirst() && f1 <= f1ZombieStd3.getSecond()) {
-                f1ZombieProb = .003;
-            } else {
-                f1ZombieProb = .0001;
-            }
-
-            if (f1HumanProb == f1ZombieProb){
-                double f1DiffHuman = Math.abs(f1 - f1HumanMean);
-                double f1DiffZombie = Math.abs(f1 - f1ZombieMean);
-                f1HumanProb = f1DiffHuman/(f1DiffHuman + f1DiffZombie);
-                f1ZombieProb = f1DiffZombie/(f1DiffHuman + f1DiffZombie);
-            }
-
             double f2HumanProb = 0;
             double f2ZombieProb = 0;
-            if (f2 >= f2HumanStd1.getFirst() && f2 <= f2HumanStd1.getSecond()) {
-                f2HumanProb = .318;
-            } else if (f2 >= f2HumanStd2.getFirst() && f2 <= f2HumanStd2.getSecond()) {
-                f2HumanProb = .046;
-            } else if (f2 >= f2HumanStd3.getFirst() && f2 <= f2HumanStd3.getSecond()) {
-                f2HumanProb = 003;
-            } else {
-                f2HumanProb = .0001;
-            }
 
-            if (f2 >= f2ZombieStd1.getFirst() && f2 <= f2ZombieStd1.getSecond()) {
-                f2ZombieProb = .318;
-            } else if (f2 >= f2ZombieStd2.getFirst() && f2 <= f2ZombieStd2.getSecond()) {
-                f2ZombieProb = .046;
-            } else if (f2 >= f2ZombieStd3.getFirst() && f2 <= f2ZombieStd3.getSecond()) {
-                f2ZombieProb = 003;
-            } else {
-                f2ZombieProb = .0001;
-            }
+            // gaussian
+            f1HumanProb = Math.exp(-Math.pow(f1 - f1HumanMean, 2) / (2 * f1HumanSTDEV)) / (Math.sqrt(2 * Math.PI * f1HumanSTDEV));
+            f1ZombieProb = Math.exp(-Math.pow(f1 - f1ZombieMean, 2) / (2 * f1ZombieSTDEV)) / (Math.sqrt(2 * Math.PI * f1ZombieSTDEV));
+            f2HumanProb = Math.exp(-Math.pow(f2 - f2HumanMean, 2) / (2 * f2HumanSTDEV)) / (Math.sqrt(2 * Math.PI * f2HumanSTDEV));
+            f2ZombieProb = Math.exp(-Math.pow(f2 - f2ZombieMean, 2) / (2 * f2ZombieSTDEV)) / (Math.sqrt(2 * Math.PI * f2ZombieSTDEV));
 
-            if (f2HumanProb == f2ZombieProb){
-                double f2DiffHuman = Math.abs(f2 - f2HumanMean);
-                double f2DiffZombie = Math.abs(f2 - f2ZombieMean);
-                f2HumanProb = f2DiffHuman/(f2DiffHuman + f2DiffZombie);
-                f2ZombieProb = f2DiffZombie/(f2DiffHuman + f2DiffZombie);
-            }
+            // System.out.println("f1HumanProb " + f1HumanProb);
+            // System.out.println("f1ZombieProb " + f1ZombieProb);
+            // System.out.println("f2HumanProb " + f2HumanProb);
+            // System.out.println("f2ZombieProb " + f2ZombieProb);
+
+            // System.out.println("f1HumanSTDEV " + f1HumanSTDEV);
+            // System.out.println("f1ZombieSTDEV " + f1ZombieSTDEV);
+            // System.out.println("f2HumanSTDEV " + f2HumanSTDEV);
+            // System.out.println("f2ZombieSTDEV " + f2ZombieSTDEV);
+            
+            // f1HumanProb = -Math.log(f1HumanProb);
+            // f1ZombieProb = -Math.log(f1ZombieProb);
+            // f2HumanProb = -Math.log(f2HumanProb);
+            // f2ZombieProb = -Math.log(f2ZombieProb);
+
+            // if (f1HumanProb < 0 || f1HumanProb > 1) {
+            //     f1HumanProb = .0001;
+            // } else {
+            //     f1HumanProb = f1HumanMean - f1HumanProb;
+            // }
+            // if (f1ZombieProb < 0 || f1ZombieProb > 1) {
+            //     f1ZombieProb = .0001;
+            // } 
+            // if (f2HumanProb < 0 || f2HumanProb > 1) {
+            //     f2HumanProb = .0001;
+            // } 
+            // if (f2ZombieProb < 0 || f2ZombieProb > 1) {
+            //     f2ZombieProb = .0001;
+            // }
+
+            // System.out.println("f1HumanProb " + f1HumanProb);
+            // System.out.println("f1ZombieProb " + f1ZombieProb);
+            // System.out.println("f2HumanProb " + f2HumanProb);
+            // System.out.println("f2ZombieProb " + f2ZombieProb);
+
+            // if (f1 >= f1HumanStd1.getFirst() || f1 <= f1HumanStd1.getSecond()) {
+            //     f1HumanProb = .318;
+            // } else if (f1 >= f1HumanStd2.getFirst() || f1 <= f1HumanStd2.getSecond()) {
+            //     f1HumanProb = .046;
+            // } else if (f1 >= f1HumanStd3.getFirst() || f1 <= f1HumanStd3.getSecond()) {
+            //     f1HumanProb = .003;
+            // } else {
+            //     f1HumanProb = .0001;
+            // }
+
+            // if (f1 >= f1ZombieStd1.getFirst() || f1 <= f1ZombieStd1.getSecond()) {
+            //     f1ZombieProb = .318;
+            // } else if (f1 >= f1ZombieStd2.getFirst() || f1 <= f1ZombieStd2.getSecond()) {
+            //     f1ZombieProb = .046;
+            // } else if (f1 >= f1ZombieStd3.getFirst() || f1 <= f1ZombieStd3.getSecond()) {
+            //     f1ZombieProb = .003;
+            // } else {
+            //     f1ZombieProb = .0001;
+            // }
+
+            // if (f1HumanProb == f1ZombieProb){
+            //     double f1DiffHuman = Math.abs(f1 - f1HumanMean);
+            //     double f1DiffZombie = Math.abs(f1 - f1ZombieMean);
+            //     f1HumanProb = f1DiffHuman/(f1DiffHuman + f1DiffZombie);
+            //     f1ZombieProb = f1DiffZombie/(f1DiffHuman + f1DiffZombie);
+            // }
+
+            // // double f2HumanProb = 0;
+            // // double f2ZombieProb = 0;
+            // if (f2 >= f2HumanStd1.getFirst() || f2 <= f2HumanStd1.getSecond()) {
+            //     f2HumanProb = .318;
+            // } else if (f2 >= f2HumanStd2.getFirst() || f2 <= f2HumanStd2.getSecond()) {
+            //     f2HumanProb = .046;
+            // } else if (f2 >= f2HumanStd3.getFirst() || f2 <= f2HumanStd3.getSecond()) {
+            //     f2HumanProb = 003;
+            // } else {
+            //     f2HumanProb = .0001;
+            // }
+
+            // if (f2 >= f2ZombieStd1.getFirst() || f2 <= f2ZombieStd1.getSecond()) {
+            //     f2ZombieProb = .318;
+            // } else if (f2 >= f2ZombieStd2.getFirst() || f2 <= f2ZombieStd2.getSecond()) {
+            //     f2ZombieProb = .046;
+            // } else if (f2 >= f2ZombieStd3.getFirst() || f2 <= f2ZombieStd3.getSecond()) {
+            //     f2ZombieProb = 003;
+            // } else {
+            //     f2ZombieProb = .0001;
+            // }
+
+            // if (f2HumanProb == f2ZombieProb){
+            //     double f2DiffHuman = Math.abs(f2 - f2HumanMean);
+            //     double f2DiffZombie = Math.abs(f2 - f2ZombieMean);
+            //     f2HumanProb = f2DiffHuman/(f2DiffHuman + f2DiffZombie);
+            //     f2ZombieProb = f2DiffZombie/(f2DiffHuman + f2DiffZombie);
+            // }
 
             double humanProb = f1HumanProb * f2HumanProb * f3HumanProbs.get(f3) * f4HumanProbs.get(f4); 
             double zombieProb = f1ZombieProb * f2ZombieProb * f3ZombieProbs.get(f3) * f4ZombieProbs.get(f4);
